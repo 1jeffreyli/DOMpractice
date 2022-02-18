57e7ce491685249ee400c277a95f154ad5a1df5a
Jeffrey Li <lijeffre@grinnell.edu>
2022-02-18 18:39:54 +0900

    Add event listeners as practice

--- script.js
+++ script.js
@@ -42,3 +42,21 @@ const pText = document.createElement('p');
         const divP = document.createElement('p');
         divP.textContent = 'ME TOO!';
         divContent.appendChild(divP);
+
+        const btn = document.querySelector('#btn');
+        btn.onclick = () => alert("Hello World");
+
+        const btn2 = document.querySelector('#btn2');
+        btn2.addEventListener('click', () => {
+        alert("Hello World");
+        });
+
+        btn.addEventListener('click', function (e) {
+            console.log(e);
+          });
+          btn.addEventListener('click', function (e) {
+            console.log(e.target);
+          });
+          btn.addEventListener('click', function (e) {
+            e.target.style.background = 'blue';
+          });
